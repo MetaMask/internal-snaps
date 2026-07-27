7ea0b51597589156b30526874e26caf9acc779f9
Hassan Malik <hbmalik88@gmail.com>
2026-07-27 17:35:02 +0200

    fix: update logic

--- packages/bitcoin-wallet-snap/src/handlers/KeyringHandler.ts
+++ packages/bitcoin-wallet-snap/src/handlers/KeyringHandler.ts
@@ -242,8 +242,8 @@ export class KeyringHandler implements KeyringSnapRpc {
 
     const account = await this.#accountsUseCases.get(accountId);
 
-    const accountType = this.#defaultAddressType;
-    if (accountType !== 'p2wpkh') {
+    const addressType = account.addressType ?? this.#defaultAddressType;
+    if (addressType !== this.#defaultAddressType) {
       throw new SnapError(
         `Only native segwit (${BtcAccountType.P2wpkh}) accounts are supported for private key export`,
       );
