a26f9e778b878930c5e455391665ab55141aeb5c
Andrew <andrew.taran@consensys.net>
2026-08-10 18:45:45 +0200

    chore: replace bitcoin logger with shared util class (#142)
    ## Explanation
    
    ### Bitcoin package update
    
    Replace bitcoin logger util with shared Logger class.
    
    <!--
    Thanks for your contribution! Take a moment to answer these questions so
    that reviewers have the information they need to properly understand
    your changes:
    
    * What is the current state of things and why does it need to change?
    * What is the solution your changes offer and how does it work?
    * Are there any changes whose purpose might not obvious to those
    unfamiliar with the domain?
    * If your primary goal was to update one package but you found you had
    to update another one along the way, why did you do so?
    * If you had to upgrade a dependency, why did you do so?
    -->
    
    ## References
    
    * Related to
    [WPN-1810](https://consensyssoftware.atlassian.net/browse/WPN-1810)
    
    <!--
    Are there any issues that this pull request is tied to?
    Are there other links that reviewers should consult to understand these
    changes better?
    Are there client or consumer pull requests to adopt any breaking
    changes?
    
    For example:
    
    * Fixes #12345
    * Related to #67890
    -->
    
    ## Checklist
    
    - [ ] I've updated the test suite for new or updated code as appropriate
    - [ ] I've updated documentation (JSDoc, Markdown, etc.) for new or
    updated code as appropriate
    - [ ] I've communicated my changes to consumers by [updating changelogs
    for packages I've
    changed](https://github.com/MetaMask/internal-snaps/tree/main/docs/processes/updating-changelogs.md)
    - [ ] I've introduced [breaking
    changes](https://github.com/MetaMask/internal-snaps/tree/main/docs/processes/breaking-changes.md)
    in this PR and have prepared draft pull requests for clients and
    consumer packages to resolve them

--- packages/bitcoin-wallet-snap/src/entities/config.ts
+++ packages/bitcoin-wallet-snap/src/entities/config.ts
@@ -1,6 +1,5 @@
 import type { AddressType, Network } from '@metamask/bitcoindevkit';
-
-import type { LogLevel } from './logger';
+import type { LogLevel } from '@metamask/snap-networks-utils/logger';
 
 export type SnapConfig = {
   logLevel: LogLevel;
