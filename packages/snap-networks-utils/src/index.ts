a6c85303f3adc0ee16dbbbc6c4e7565f24d953e1
Andrew Taran <andrew.taran@consensys.net>
2026-08-19 13:01:52 +0200

    chore: add index export for logger

--- packages/snap-networks-utils/src/index.ts
+++ packages/snap-networks-utils/src/index.ts
@@ -9,3 +9,11 @@ export {
   type RemoteFeatureFlagsProviderMessenger,
 } from './providers/remote-feature-flags/RemoteFeatureFlagsProvider';
 export { safeMerge } from './safeMerge/safeMerge';
+export { Logger, LogLevel } from './logger';
+export type {
+  LoggerOptions,
+  LoggerMethod,
+  LogMethod,
+  LogMethodDecorator,
+  LoggerDecorators,
+} from './logger';
