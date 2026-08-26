5eb27b0a7134789dc4948aa2716bf4f2d2f2f994
Andrew Taran <andrew.taran@consensys.net>
2026-08-26 16:17:53 +0200

    feat: add batches utils into snap-networks-utils

--- packages/snap-networks-utils/src/index.ts
+++ packages/snap-networks-utils/src/index.ts
@@ -13,6 +13,13 @@ export { buildUrl } from './buildUrl/buildUrl';
 export type { BuildUrlParams } from './buildUrl/buildUrl';
 export { sanitizeControlCharacters, sanitizeUri } from './sanitize';
 export { UrlStruct } from './urlStruct/urlStruct';
+export {
+  batchesAll,
+  batchesAllSettled,
+  batchesAllSettledWithChunks,
+  batchesAllWithChunks,
+  chunks,
+} from './async';
 export { Logger, LogLevel } from './logger';
 export type {
   LoggerOptions,
