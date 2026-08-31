a47ee48cfd097dd8263c5b456bf69f970666a2ce
Andrew Taran <andrew.taran@consensys.net>
2026-08-31 14:03:20 +0200

    feat: move withCatchAndThrowSnapErrorHandler into shared lib

---
+++ packages/snap-networks-utils/src/errors/index.ts
@@ -0,0 +1,8 @@
+export { createWithCatchAndThrowSnapError, normalizeError } from './errors';
+export { isSnapRpcError } from './snapRpcError';
+export type {
+  CreateWithCatchAndThrowSnapErrorOptions,
+  NormalizeErrorFn,
+  TrackErrorFn,
+} from './errors';
+export type { SnapRpcError } from './snapRpcError';
