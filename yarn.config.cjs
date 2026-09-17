91ded230247b32cff002de8f1366d5f8848f6fd4
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2026-09-17 17:20:31 +0800

    Update yarn.config.cjs

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -200,32 +200,36 @@ module.exports = defineConfig({
           '../../scripts/since-latest-release.sh',
         );
 
-        // All non-root packages must have the same "test" script.
-        expectWorkspaceField(
+        // All non-root packages must run Jest with the silent reporter. Extra
+        // prefixes (e.g. `NODE_OPTIONS=...`) are allowed.
+        expectWorkspaceFieldRegex(
           workspace,
           'scripts.test',
-          'NODE_OPTIONS=--experimental-vm-modules jest --reporters=jest-silent-reporter',
+          /jest --reporters=jest-silent-reporter$/u,
         );
 
-        // All non-root packages must have the same "test:clean" script.
-        expectWorkspaceField(
+        // All non-root packages must have a "test:clean" script that clears the
+        // Jest cache. Extra prefixes (e.g. `NODE_OPTIONS=...`) are allowed.
+        expectWorkspaceFieldRegex(
           workspace,
           'scripts.test:clean',
-          'NODE_OPTIONS=--experimental-vm-modules jest --clearCache',
+          /jest --clearCache$/u,
         );
 
-        // All non-root packages must have the same "test:verbose" script.
-        expectWorkspaceField(
+        // All non-root packages must have a "test:verbose" script that runs Jest
+        // in verbose mode. Extra prefixes (e.g. `NODE_OPTIONS=...`) are allowed.
+        expectWorkspaceFieldRegex(
           workspace,
           'scripts.test:verbose',
-          'NODE_OPTIONS=--experimental-vm-modules jest --verbose',
+          /jest --verbose$/u,
         );
 
-        // All non-root packages must have the same "test:watch" script.
-        expectWorkspaceField(
+        // All non-root packages must have a "test:watch" script that runs Jest
+        // in watch mode. Extra prefixes (e.g. `NODE_OPTIONS=...`) are allowed.
+        expectWorkspaceFieldRegex(
           workspace,
           'scripts.test:watch',
-          'NODE_OPTIONS=--experimental-vm-modules jest --watch',
+          /jest --watch$/u,
         );
       }
 
@@ -500,6 +504,47 @@ function expectWorkspaceField(workspace, fieldName, expectedValue = undefined) {
     (fieldValue === undefined || fieldValue === null)
   ) {
     workspace.error(`Missing required field "${fieldName}".`);
+  }
+}
+
+/**
+ * This function does one of two things depending on the arguments given:
+ *
+ * - With no regex provided, this will expect that the workspace has the given
+ * field and that it is a non-null value; if the field is not present or is
+ * null, this will log an error and cause the constraint to fail.
+ * - With a regex provided, this will also verify that the field is a string
+ * that matches the given regular expression. Unlike `expectWorkspaceField`,
+ * this never auto-fixes the field, because a regex cannot produce a unique
+ * replacement value.
+ *
+ * @param {Workspace} workspace - The workspace to check.
+ * @param {string} fieldName - The field to check.
+ * @param {RegExp} [regex] - The regular expression the field value must match.
+ */
+function expectWorkspaceFieldRegex(workspace, fieldName, regex = undefined) {
+  const fieldValue = get(workspace.manifest, fieldName);
+
+  if (fieldValue === undefined || fieldValue === null) {
+    workspace.error(`Missing required field "${fieldName}".`);
+    return;
+  }
+
+  if (regex === undefined) {
+    return;
+  }
+
+  if (typeof fieldValue !== 'string') {
+    workspace.error(
+      `Expected field "${fieldName}" to be a string, but got ${typeof fieldValue}.`,
+    );
+    return;
+  }
+
+  if (!regex.test(fieldValue)) {
+    workspace.error(
+      `Expected field "${fieldName}" to match ${inspect(regex)}, but found "${fieldValue}".`,
+    );
   }
 }
 
